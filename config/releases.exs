aa496b4589c7aa60a7ac624d59ac9d5e7f571cb3
eli venus <eli@venus.com>
2019-07-29 19:08:52 +0300

    setup release

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,7 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -24,7 +24,7 @@ secret_key_base =
     """
 
 config :chatlag, ChatlagWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "9040")],
   secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
@@ -32,7 +32,7 @@ config :chatlag, ChatlagWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :chatlag, ChatlagWeb.Endpoint, server: true
+    config :chatlag, ChatlagWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
