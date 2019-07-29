e0abf8691fd0e6a28e97c85b636b3a4736f114d5
eli venus <eli@venus.com>
2019-07-29 20:46:35 +0300

    adding users

---
+++ priv/repo/migrations/20190729173303_create_users.exs
@@ -0,0 +1,19 @@
+defmodule Chatlag.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :full_name, :string, null: true, size: 60
+      add :nickname, :string, null: false
+      add :email, :string, null: true, size: 60
+      add :role, :string, null: false, default: "visitor"
+      add :age, :integer, null: false
+      add :gender, :string, null: false, size: 2
+      add :ip_address, :string, size: 20, null: true
+      add :suspend_at, :date, null: true
+
+      timestamps()
+    end
+
+  end
+end
