e02c1303c7ef9adbc4f0a4b702cd1aaf2038f689
eli venus <eli@venus.com>
2019-08-04 18:50:21 +0300

    fixing css

--- priv/repo/migrations/20190730165250_create_rooms.exs
+++ priv/repo/migrations/20190730165250_create_rooms.exs
@@ -4,6 +4,7 @@ defmodule Chatlag.Repo.Migrations.CreateRooms do
   def change do
     create table(:rooms) do
       add :title, :string
+      add :attached, :string
       add :slug, :string
       add :on_front, :boolean, default: false, null: false
       add :small_desc, :text
