1d629fcb2ce1328524ddee4dc595e59abd2059f5
Eli Hayun <eli@venus.lan>
2019-09-17 09:45:37 +0300

    adding search and fix display

--- lib/chatlag_web/controllers/lobby_controller.ex
+++ lib/chatlag_web/controllers/lobby_controller.ex
@@ -1,8 +1,47 @@
 defmodule ChatlagWeb.LobbyController do
   use ChatlagWeb, :controller
+  import Ecto.Query
+
+  alias Chatlag.Repo
+
+  alias Chatlag.Chat.Room
 
   plug :put_layout, "empty.html" when action in [:index]
-  def index(conn, _params) do
+
+  def index(conn, params) do
+    search = params["search"]
+
+    if search do
+      search_term = "%#{params["search"]}%"
+
+      searchQuery =
+        Repo.all(
+          from r in Room,
+            select: %{id: r.id},
+            where:
+              like(r.title, ^search_term) or
+                like(r.slogen, ^search_term) or
+                like(r.small_desc, ^search_term)
+        )
+
+      IO.inspect(searchQuery, label: "params")
+
+      case searchQuery do
+        [] ->
+          IO.puts("Nothing found")
+
+        nil ->
+          IO.puts("Nothing found")
+
+        _ ->
+          room = searchQuery |> Enum.at(0)
+
+          conn
+          |> redirect(to: "/chat/#{room.id}")
+          |> halt
+      end
+    end
+
     user_id = get_session(conn, :user_id)
 
     current_user = Pow.Plug.current_user(conn)
