abae0a383126ccbad5ff6afd3dfcb472a2386777
Eli Hayun <eli@venus.lan>
2019-09-23 05:49:28 +0300

    fix social icons

--- lib/chatlag_web/views/chat_view.ex
+++ lib/chatlag_web/views/chat_view.ex
@@ -126,7 +126,7 @@ defmodule ChatlagWeb.ChatView do
   # https://wa.me/?text=Chatlaghttp%3A%2F%2Fwww.efrat37.co.il%2F%23%2Fchat%3Froom%3D%25D7%25A4%25D7%2595%25D7%259C%25D7%2599%25D7%2598%25D7%2599%25D7%25A7%25D7%2594
   # https://twitter.com/intent/tweet?text=Chatlag%20http%3A%2F%2Fwww.efrat37.co.il%2F%23%2Fchat%3Froom%3D%25D7%25A4%25D7%2595%25D7%259C%25D7%2599%25D7%2598%25D7%2599%25D7%25A7%25D7%2594
   # https://facebook.com/sharer.php?u=http%3A%2F%2Fwww.efrat37.co.il%2F%23%2Fchat%3Froom%3D%25D7%25A4%25D7%2595%25D7%259C%25D7%2599%25D7%2598%25D7%2599%25D7%25A7%25D7%2594
-  def shareMessage(url, media, msg) do
+  def shareMessage(url, media, msg \\ "") do
     thisUrl = "https://chatlag.co.il#{url}"
     # https://api.whatsapp.com/send?text=%22kjkj%20kj%20kjk%22&url=lll.com
     link =
@@ -135,7 +135,7 @@ defmodule ChatlagWeb.ChatView do
           "https://facebook.com/sharer.php?u=#{thisUrl}&message=#{msg}"
 
         :wp ->
-          "https://api.whatsapp.com/send?text=#{msg} #{thisUrl}&url=https://chatlag.co.il"
+          "https://api.whatsapp.com/send?text=#{thisUrl}  #{msg} "
 
         :twitter ->
           "https://twitter.com/intent/tweet?text=#{thisUrl} #{msg}"
