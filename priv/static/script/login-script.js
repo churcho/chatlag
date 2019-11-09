673313352b16ef06b24f089256617a46481ea9b7
Eli Hayun <eli@venus.lan>
2019-11-09 04:55:52 +0200

    open login form

--- priv/static/script/login-script.js
+++ priv/static/script/login-script.js
@@ -1,34 +1,34 @@
- var isForm = false;
- var minAge = 12;
- var maxAge = 120;
- var isAge = false;
+var isForm = true;
+var minAge = 12;
+var maxAge = 120;
+var isAge = false;
 
-$(document).ready(function() {
+$(document).ready(function () {
 	"use strict";
-	$(".login-anonymous-button").click(function(e) {
+	$(".login-anonymous-button").click(function (e) {
 		if (!isForm) {
 			e.preventDefault();
 			toggleForm();
 		}
 	});
-	
-	$(".arrow-up").click(function() {
+
+	$(".arrow-up").click(function () {
 		toggleForm();
 	});
-	
+
 	function toggleForm() {
-		isForm = !isForm;
-		$(".hidden-form").toggleClass("showloginform");
-		$(".arrow-up").toggleClass("flip");
-		$(".login-col").toggleClass("fixlogincol");
+		// isForm = !isForm;
+		// $(".hidden-form").toggleClass("showloginform");
+		// $(".arrow-up").toggleClass("flip");
+		// $(".login-col").toggleClass("fixlogincol");
 	}
 
-	$(".select").on("change", function() {
+	$(".select").on("change", function () {
 		$(this).removeClass("lightgray");
 	});
-	
-	for (var i=minAge;i<=maxAge;i++) {
-		$(".select").html($(".select").html()+'<option class="black" value="'+i+'">'+i+'</option>');
+
+	for (var i = minAge; i <= maxAge; i++) {
+		$(".select").html($(".select").html() + '<option class="black" value="' + i + '">' + i + '</option>');
 	}
 });
 
